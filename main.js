9a98068720479b17f7b2b2d88fd316e3aa29daaa
VanillaTank <tedien2016@yandex.ru>
2021-04-02 15:48:06 +0800

    12

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ window.onload = () => {
 
     //сохранение изменений
     const saveData = () => {
-        localStorage.remove('MyTrackerData')
+        localStorage.removeItem('MyTrackerData')
         alert('Данные удалены')
         let newData = [];
         if (listContainer.children[0].innerText !== "Нет данных в хранилище") {
